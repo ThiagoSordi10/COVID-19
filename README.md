13720e14c2d595025e025c4e7ebfb7ad0453729d
Thiago Sordi <thiagosordi10@gmail.com>
2020-11-13 15:17:01 -0300

    Adaptação do código do Jupyter para arquivos python
    Código testado será implementado em dois arquivos, um para treinar e gerar o modelo e o outro para prever para o usuário

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Predizer o número de casos de COVID-19 mundial
  Primeiro será baixado o ambiente para construção e treino do modelo - '''docker pull jupyter/datascience-notebook'''.
 
 Em seguida, o comando para rodar o ambiente para desenvolvimento
- '''docker run --rm -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v "%cd%":/home/jovyan/work jupyter/datascience-notebook'''
+ '''docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "%cd%":/home/jovyan/work jupyter/datascience-notebook'''
+
+ Será gerada uma URL com token, basta copiá-la e acessar os arquivos.
 
  Link de apoio: https://jupyter-docker-stacks.readthedocs.io/en/latest/index.html
 
@@ -14,6 +16,6 @@ Em seguida, o comando para rodar o ambiente para desenvolvimento
  <h4>A ideia era verificar a relação dos dados e tentar montar uma
  forma onde pudesse utilizar o próprio número de novos casos ao longo dos dias para ter uma previsão do número de novos casos de um dia. Para isso se criou um array Numpy com tamanho da quantidade de dias até o atual momento, e em cada posição foi colocado um outro array contendo números de casos do dia/index do array atual até 9 dias a frente. Exemplo: Na posição 0 do Numpy array, que seria o primeiro dia com caso registrado no mundo, recebe um outro array contendo o número de novos casos desse primeiro dia e dos próximos 9. Esse valor 9 é arbitrário, foram testados alguns outros, mas o 9 teve bons resultados. </h4>
 
- <h4>Bom, já temos os dados para treinar e testar e temos os labels para validar, que são o número de novos casos no dia simplesmente. Também de forma arbitrária foi escolhido o valor de 67% do conjunto de dados para treino e 33% para testes, apenas por ter dado bons resultados. Foram testados alguns modelos de regressão linear do SKLearn, como: LinearRegression, GradientBoostingRegressor, RidgeCV, entre outros. Mas o que mais se destacou foi o Lars. O score do modelo ficou na faixa de 97%. </h4>
+ <h4>Bom, já temos os dados para treinar e testar, e temos os labels para validar, que são o número de novos casos no dia simplesmente. Também de forma arbitrária foi escolhido o valor de 67% do conjunto de dados para treino e 33% para testes, apenas por ter dado bons resultados. Foram testados alguns modelos de regressão linear do SKLearn, como: LinearRegression, GradientBoostingRegressor, RidgeCV, entre outros. Mas o que mais se destacou foi o Lars. O score do modelo ficou na faixa de 97%. </h4>
 
  <h4>O modelo então é salvo em um arquivo, para que seja carregado e utilizado quando o programa que o usuário executa para prever os novos casos ao longo dos dias.</h4>
---
+++ build_model.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import Lars
+from sklearn import svm
+
+def build_df():
+    print("Lendo dados")
+    df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+    df = df[df.location == "World"] # take just data grouped already (world)
+    df.drop(['iso_code', 'continent', 'location'], axis=1, inplace=True)
+    df = df.loc[:, ['date','new_cases']] #Just need these three columns
+    df = df.reset_index(drop=True)
+    print("Dados lidos")
+    return df
+
+def prepare_data(df):
+    print("Preparando dados")
+    # Cases will be used as input for the model
+    cases = df['new_cases'].to_numpy()
+
+    # Build an array of days to use to plot and when the user enter with the number of days ahead to predict
+    days = []
+    count = 0
+    for elements in cases:
+      count += 1
+      days.append(count)
+    df['days'] = days
+
+    days_after = 9
+    size = (len(cases) , days_after)
+    x = np.zeros(size)
+    y = np.zeros(len(cases))
+
+    # The idea is to use a group cases of many days to predict the number of cases of one single day
+    for i in range(len(cases) - days_after):
+      temp = np.zeros(days_after)
+      # Take the days_after's value of elements since i, for cases
+      temp[0 : days_after] = cases[i : i+ days_after]
+      x[i] = temp
+
+      y[i] = cases[i+days_after] # take the number of cases as labels
+
+
+    # Drop the last n days_after of arrays because it only gets 0
+    x = x[0:len(df) - days_after]
+    y = y[0:len(df) - days_after]
+    # Then drop the value of number days since this values aren't in the x and y anymore
+    days = days[0:len(df) - days_after]
+
+    split = int(0.67*len(x))
+    days = days[split:]
+    print("Dados preparados")
+    return x, y, split
+
+def build_model(df):
+    print("Montando modelo")
+    x, y, split = prepare_data(df)
+    model = Lars(n_nonzero_coefs=1000)
+    X_train, X_test, y_train, y_test = x[:split], x[split:], y[:split], y[split:]
+    # X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+    model.fit(X_train, y_train)
+    print("{}".format(np.linalg.norm(model.predict(X_test) - y_test, 1)/len(y_test)))
+    predict = model.predict(X_test)
+    cross_val_score(model, X_train, y_train, cv = 2)
+    print("Modelo montado")
+    return model
+
+def save_model(model):
+    # save the model to disk
+    filename = 'model.sav'
+    pickle.dump(model, open(filename, 'wb'))
+
+
+df = build_df()
+model = build_model(df)
+save_model(model)
+print("FIM")
